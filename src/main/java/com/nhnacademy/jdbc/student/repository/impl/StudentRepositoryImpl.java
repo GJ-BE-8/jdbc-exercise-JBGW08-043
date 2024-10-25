2bf1d28ba39e2f4ed72ebabe7e56120b577b24b2
조승주 <tmdwn3318@gmail.com>
2024-10-25 10:22:17 +0900

    완

--- src/main/java/com/nhnacademy/jdbc/student/repository/impl/StudentRepositoryImpl.java
+++ src/main/java/com/nhnacademy/jdbc/student/repository/impl/StudentRepositoryImpl.java
@@ -12,12 +12,49 @@ public class StudentRepositoryImpl implements StudentRepository {
     @Override
     public int save(Connection connection, Student student){
         //todo#2 학생등록
-        return 0;
+
+        String sql = "insert into jdbc_students(id, name, gender, age) values (?, ?, ?, ?)";
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, student.getId());
+            stmt.setString(2, student.getName());
+            stmt.setString(3, student.getGender().toString());
+            stmt.setInt(4, student.getAge());
+            return stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
-    public Optional<Student> findById(Connection connection,String id){
+    public Optional<Student> findById(Connection connection, String id){
         //todo#3 학생조회
+        String sql = "select * from jdbc_students where id = ?";
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, id);
+            ResultSet rs =  stmt.executeQuery();
+
+            if(rs.next()){
+                Student student = new Student(
+                        rs.getString("id"),
+                        rs.getString("name"),
+                        Student.GENDER.valueOf(rs.getString("gender")),
+                        rs.getInt("age"),
+                        rs.getTimestamp("created_at").toLocalDateTime()
+                );
+
+                return Optional.of(student);
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
         return Optional.empty();
     }
@@ -25,15 +62,34 @@ public class StudentRepositoryImpl implements StudentRepository {
     @Override
     public int update(Connection connection,Student student){
         //todo#4 학생수정
+        String sql = "update jdbc_students set name = ?, gender = ?, age = ? where id = ?";
 
-        return 0;
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, student.getName());
+            stmt.setString(2, student.getGender().toString());
+            stmt.setInt(3, student.getAge());
+            stmt.setString(4, student.getId());
+            return stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public int deleteById(Connection connection,String id){
         //todo#5 학생삭제
+        String sql = "delete from jdbc_students where id = ?";
 
-        return 0;
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, id);
+            return stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
