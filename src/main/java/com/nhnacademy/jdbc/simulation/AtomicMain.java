fc25bf9f70e01f9e0d234cd0a0d3c6c19513ec19
조승주 <tmdwn3318@gmail.com>
2024-10-25 10:39:00 +0900

    1

--- src/main/java/com/nhnacademy/jdbc/simulation/AtomicMain.java
+++ src/main/java/com/nhnacademy/jdbc/simulation/AtomicMain.java
@@ -22,21 +22,21 @@ public class AtomicMain {
         connection.setAutoCommit(false);
         connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
 
-        //todo1 - 계좌생성 : 8000
+        //todo 1 계좌생성 : 8000
         bankService.createAccount(connection,new Account(8000l,"nhn아카데미-8000",10_0000l ));
 
         try{
-            //todo2 - 8000계좌에서 -> 5_0000인출
+            //todo 2 - 8000계좌에서 -> 5_0000인출
             bankService.withdrawAccount(connection,8000l,5_0000);
 
-            //todo3 - 9000 계좌에 -> 5_0000입급
+            //todo 3 - 9000 계좌에 -> 5_0000입급
             bankService.depositAccount(connection,9000l,5_0000);
             //9000 계좌는 존재하지 않음으로 AccountNotFoundException 예외발생
 
             connection.commit();
         }catch (Exception e){
             log.debug("withdraw:{}",e.getMessage());
-            //todo4 - rollback
+            //todo 4 - rollback
             connection.rollback();
         }
         
